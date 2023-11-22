d8072579e50cb3369a46a5a055884bc2135d2546
Luke Marsden <lhmarsden@hotmail.com>
2023-11-22 13:12:32 +0100

    Automatic commit and push after building new report

--- build_new_report.sh
+++ build_new_report.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source utils/copy_from_ppi.sh
-source utils/build.sh
-source utils/git_push.sh
+# Get the directory of the currently executing script
+repo_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source $repo_dir/utils/copy_from_ppi.sh
+source $repo_dir/utils/build.sh
+source $repo_dir/utils/git_push.sh
